395fec84e11e93817580c8377c090fb8916c1f0a
carpediem <luvrel@naver.com>
2024-01-08 16:54:54 +0900

    Initial commit

---
+++ src/main/java/com/team5/projrental/ProjrentalApplication.java
@@ -0,0 +1,13 @@
+package com.team5.projrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjrentalApplication.class, args);
+	}
+
+}
