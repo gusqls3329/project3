9cf9681a3a5ae0f2e50a7bd2ba20ca26e213fa22
carpediem <luvrel@naver.com>
2024-01-08 19:48:04 +0900

    chat패키지 및 간단한 내용

---
+++ src/main/java/com/team5/projrental/chat/ChatMapper.java
@@ -0,0 +1,8 @@
+package com.team5.projrental.chat;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ChatMapper {
+
+}
---
+++ src/main/java/com/team5/projrental/chat/ChatService.java
@@ -0,0 +1,14 @@
+package com.team5.projrental.chat;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+
+    private final ChatMapper mapper;
+
+}
