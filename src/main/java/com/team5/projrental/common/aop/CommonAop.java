3f63a0c1fe1a036529ef967770568daf15a11aae
devYule <privateDevYule@gmail.com>
2024-01-10 07:18:13 +0900

    2024-01-10 -by hyunmin

--- src/main/java/com/team5/projrental/common/aop/CommonAop.java
+++ src/main/java/com/team5/projrental/common/aop/CommonAop.java
@@ -1,19 +1,16 @@
 package com.team5.projrental.common.aop;
 
-import com.team5.projrental.common.threadpool.MyThreadPool;
-import com.team5.projrental.product.ProductMapper;
+import com.team5.projrental.common.threadpool.MyThreadPoolHolder;
 import com.team5.projrental.product.ProductRepository;
 import com.team5.projrental.product.model.CurProductListVo;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 
 @Aspect
 @Slf4j
@@ -23,7 +20,7 @@ public class CommonAop {
     private final ProductRepository productRepository;
     private final ExecutorService threadPool;
 
-    public CommonAop(ProductRepository productRepository, MyThreadPool threadPool) {
+    public CommonAop(ProductRepository productRepository, MyThreadPoolHolder threadPool) {
         this.productRepository = productRepository;
         this.threadPool = threadPool.getThreadPool();
     }
--- src/main/java/com/team5/projrental/common/threadpool/MyThreadPoolHolder.java
+++ src/main/java/com/team5/projrental/common/threadpool/MyThreadPoolHolder.java
@@ -1,21 +1,19 @@
 package com.team5.projrental.common.threadpool;
 
-import com.team5.projrental.common.Const;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Component
 @Getter
-public class MyThreadPool {
+public class MyThreadPoolHolder {
 
     public final ExecutorService threadPool;
 
-    public MyThreadPool(@Value("${server.tomcat.threads.max}") Integer originalThreadPoolSize) {
+    public MyThreadPoolHolder(@Value("${server.tomcat.threads.max}") Integer originalThreadPoolSize) {
         this.threadPool = Executors.newFixedThreadPool(originalThreadPoolSize / 2);
     }
 }
