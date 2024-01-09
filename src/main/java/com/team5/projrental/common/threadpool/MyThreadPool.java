de4a31e647b22f2fbc33aac625925650cb5144a4
devYule <privateDevYule@gmail.com>
2024-01-10 07:08:35 +0900

    2024-01-10 -by hyunmin

--- src/main/java/com/team5/projrental/common/threadpool/MyThreadPool.java
+++ src/main/java/com/team5/projrental/common/threadpool/MyThreadPool.java
@@ -2,6 +2,7 @@ package com.team5.projrental.common.threadpool;
 
 import com.team5.projrental.common.Const;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,7 +15,7 @@ public class MyThreadPool {
 
     public final ExecutorService threadPool;
 
-    public MyThreadPool() {
-        this.threadPool = Executors.newFixedThreadPool(Const.MY_THREAD_POOL_SIZE);
+    public MyThreadPool(@Value("${server.tomcat.threads.max}") Integer originalThreadPoolSize) {
+        this.threadPool = Executors.newFixedThreadPool(originalThreadPoolSize / 2);
     }
 }
