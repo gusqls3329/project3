accf663e2a621a7c0d7627fae3fd6eaafc4f7b72
carpediem <luvrel@naver.com>
2024-01-16 15:31:30 +0900

    상원 마이페이지 서비스 하이주석

--- src/main/java/com/team5/projrental/chat/model/ChatInsDto.java
+++ src/main/java/com/team5/projrental/chat/model/ChatInsDto.java
@@ -2,14 +2,21 @@ package com.team5.projrental.chat.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class ChatInsDto {
 
+    @Length(min = 1)
     private int ichat;
+
+    @Length(min = 1)
     private int iproduct;
 
     @JsonIgnore
+    @Length(min = 1)
     private int loginedIuser;
+
+    @Length(min = 1)
     private int otherPersonIuser;
 }
--- src/main/java/com/team5/projrental/chat/model/ChatMsgSelDto.java
+++ src/main/java/com/team5/projrental/chat/model/ChatMsgSelDto.java
@@ -2,13 +2,22 @@ package com.team5.projrental.chat.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class ChatMsgSelDto {
+
+    @Length(min = 1)
     private int page;
+
+    @Length(min = 1)
     private int ichat;
+
     @JsonIgnore
+    @Length(min = 1)
     private int startIdx;
+
+    @Length(min = 1)
     private int rowCount = 20;
 
     public void setPage(int page) {
