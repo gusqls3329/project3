f18ef13e16f39137131f01ddf90bc19b28bffc85
김현빈 <gusqls81017@gmail.com>
2024-01-17 18:09:52 +0900

    서비스

--- src/main/java/com/team5/projrental/user/model/UserCheckInfoDto.java
+++ src/main/java/com/team5/projrental/user/model/UserCheckInfoDto.java
@@ -2,8 +2,10 @@ package com.team5.projrental.user.model;
 
 import com.team5.projrental.common.exception.ErrorCode;
 import com.team5.projrental.common.exception.ErrorMessage;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.Range;
 
 import static com.team5.projrental.common.exception.ErrorCode.*;
@@ -12,8 +14,12 @@ import static com.team5.projrental.common.exception.ErrorCode.*;
 public class UserCheckInfoDto {
 
     @NotNull(message = ErrorMessage.CAN_NOT_BLANK_EX_MESSAGE)
-    @Range(min = 1, max = 2)
+    @Range(min = 1, max = 2, message = ErrorMessage.ILLEGAL_RANGE_EX_MESSAGE)
     private Integer div;
+    @NotBlank(message = ErrorMessage.CAN_NOT_BLANK_EX_MESSAGE)
+    @Length(min = 8, max = 15, message = ErrorMessage.ILLEGAL_RANGE_EX_MESSAGE)
     private String uid;
+    @NotBlank(message = ErrorMessage.CAN_NOT_BLANK_EX_MESSAGE)
+    @Length(max = 20, message = ErrorMessage.ILLEGAL_RANGE_EX_MESSAGE)
     private String nick;
 }
