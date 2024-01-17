95d1e70c0428d65d7ce6c9cd54dab28b26d87a9d
carpediem <luvrel@naver.com>
2024-01-17 18:08:26 +0900

    상원

--- src/main/java/com/team5/projrental/user/model/UserSelDto.java
+++ src/main/java/com/team5/projrental/user/model/UserSelDto.java
@@ -1,9 +1,15 @@
 package com.team5.projrental.user.model;
 
+import com.team5.projrental.common.exception.ErrorMessage;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class UserSelDto {
+    @NotBlank(message = ErrorMessage.CAN_NOT_BLANK_EX_MESSAGE)
     private String uid;
+
+    @Range(min = 1)
     private int iuser;
 }
