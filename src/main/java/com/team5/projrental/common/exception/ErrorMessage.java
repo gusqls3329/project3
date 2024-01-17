6f4220d40e628c39edbb0603b7fdb03a04b869b9
김현빈 <gusqls81017@gmail.com>
2024-01-17 17:48:09 +0900

    서비스

--- src/main/java/com/team5/projrental/common/exception/ErrorMessage.java
+++ src/main/java/com/team5/projrental/common/exception/ErrorMessage.java
@@ -31,6 +31,7 @@ public interface ErrorMessage {
     String BAD_DIV_INFO_EX_MESSAGE = "div 값이 잘못되었습니다.";
     String BAD_PIC_EX_MESSAGE = "사진이 존재하지 않습니다.";
     String BAD_ID_EX_MESSAGE = "이미 존재하는 아이디 입니다.";
+    String BAD_NICK_EX_MESSAGE = "이미 존재하는 닉네임 입니다.";
     String BAD_PRODUCT_INFO_EX_MESSAGE = "잘못된 제품 정보 입니다.";
     //
     String ALL_INFO_NOT_EXISTS_EX_MESSAGE = "모든 정보가 제공되지 않음";
