f119bf1b60e8a492b783cf334b88b3b33461212e
김현빈 <gusqls81017@gmail.com>
2024-01-19 16:42:06 +0900

    서비스

--- src/main/java/com/team5/projrental/payment/review/model/UpRieDto.java
+++ src/main/java/com/team5/projrental/payment/review/model/UpRieDto.java
@@ -3,17 +3,17 @@ package com.team5.projrental.payment.review.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.team5.projrental.common.exception.ErrorMessage;
 import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UpRieDto {
     private int ireview;
 
-
     private String contents;
 
     @Max(value = 5, message = ErrorMessage.ILLEGAL_RANGE_EX_MESSAGE)
     private Integer rating;
+
+    @JsonIgnore
+    private int iuser;
 }
