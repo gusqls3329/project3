fe7f13f6e0ff26c85f0e8833dfd558e4f31635cd
carpediem <luvrel@naver.com>
2024-01-22 17:49:42 +0900

    2024-01-22 --by sangwun

--- src/main/java/com/team5/projrental/user/model/SigninDto.java
+++ src/main/java/com/team5/projrental/user/model/SigninDto.java
@@ -1,6 +1,8 @@
 package com.team5.projrental.user.model;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import com.team5.projrental.common.exception.ErrorMessage;
 import lombok.Data;
@@ -10,9 +12,11 @@ import org.hibernate.validator.constraints.Length;
 public class SigninDto {
     @NotBlank(message = ErrorMessage.CAN_NOT_BLANK_EX_MESSAGE)
     @Length(min = 8, max = 15, message = ErrorMessage.ILLEGAL_RANGE_EX_MESSAGE)
+    @Schema(defaultValue = "qwqwqw11")
     private String uid;
 
     @NotBlank(message = ErrorMessage.CAN_NOT_BLANK_EX_MESSAGE)
     @Length(min = 8, max = 20, message = ErrorMessage.ILLEGAL_RANGE_EX_MESSAGE)
+    @Schema(defaultValue = "12121212")
     private String upw;
 }
