dd45cb9c606ae5debdaf155d47803745686c4ab1
jo <yuyule20@naver.com>
2024-01-25 17:04:29 +0900

    2024-01-25 --by Hyunmin

--- src/main/java/com/team5/projrental/common/aop/SseEmitterAspect.java
+++ src/main/java/com/team5/projrental/common/aop/SseEmitterAspect.java
@@ -38,10 +38,11 @@ public class SseEmitterAspect {
 
     }
 
-    // aop 로 추상화 가능할까?
 
     // 리뷰 작성시점에 상대유저에게 푸시 보내기 위한 이벤트 발생
     // 만약 로그인유저가 리뷰를 썼는데, 상대 유저가 리뷰를 썼다면 거래완료 메시지와 코드를 푸시
+    // + 만약 해당 유저의 SseEmitter 가 존재하지 않으면 DB 에 저장해두고, 로그인시 해당 데이터 일괄 보내기
+    // 여기서 필요한건 SseEmitter 가 존재하지 않으면 DB 에 저장.
     @AfterReturning("postReview() && args(dto)")
     public void makeEvent(JoinPoint joinPoint, RivewDto dto) {
         threadPool.execute(() -> {
--- src/main/java/com/team5/projrental/common/sse/SseEmitterMapper.java
+++ src/main/java/com/team5/projrental/common/sse/SseEmitterMapper.java
@@ -1,11 +1,20 @@
 package com.team5.projrental.common.sse;
 
 import com.team5.projrental.common.sse.model.FindDiffUserDto;
+import com.team5.projrental.common.sse.model.RejectMessageInfo;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface SseEmitterMapper {
     int findExpiredPaymentCountBy(int iuser);
 
     FindDiffUserDto findDiffUserBy(int ipayment, int iuser);
+
+    List<RejectMessageInfo> findRejectedMessage(int iuser);
+
+    int deleteRejectedMessage(int iuser);
+
+    int deleteRejectedMessage(RejectMessageInfo dto);
 }
