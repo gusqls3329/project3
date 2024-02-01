6bc62416c4f60a8fadc6ee8249aad40a3b72f622
carpediem <luvrel@naver.com>
2024-02-01 16:21:32 +0900

    2024-02-01 sanwon Errorcode and chat

--- src/main/java/com/team5/projrental/mypage/MypageController.java
+++ src/main/java/com/team5/projrental/mypage/MypageController.java
@@ -6,8 +6,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import jakarta.validation.constraints.Min;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +31,7 @@ public class MypageController {
     @Parameters(value = {
             @Parameter(name = "page", description = "페이지"),
             @Parameter(name = "role", description = "role : 1 = 빌린 내역, 2 = 빌려준 내역")})
-    public List<PaymentSelVo> getPaymentList(@RequestParam int page, @RequestParam int role) {
+    public List<PaymentSelVo> getPaymentList(@RequestParam(defaultValue = "1") @Range(min = 1) int page, @RequestParam @Range(min = 1, max = 2) int role) {
         PaymentSelDto dto = new PaymentSelDto();
         dto.setPage(page);
         dto.setRole(role);
@@ -40,7 +42,7 @@ public class MypageController {
     @GetMapping("/review")
     @Operation(summary = "로그인 유저가 작성한 후기", description = "로그인 유저가 빌린내역 중 작성된 후기")
     @Parameters(value = {@Parameter(name = "page", description = "페이지")})
-    public List<MyBuyReviewListSelVo> getReview(@RequestParam @Min(1) int page) {
+    public List<MyBuyReviewListSelVo> getReview(@RequestParam(defaultValue = "1") @Min(1) int page) {
         MyBuyReviewListSelDto dto = new MyBuyReviewListSelDto();
         dto.setPage(page);
         return service.selIbuyerReviewList(dto);
@@ -50,7 +52,7 @@ public class MypageController {
     @GetMapping("/fav")
     @Operation(summary = "로그인 유저가 찜한 목록", description = "로그인 유저가 찜한 목록")
     @Parameters(value = {@Parameter(name = "page", description = "페이지")})
-    public List<MyFavListSelVo> getFavList(@RequestParam @Min(1) int page) {
+    public List<MyFavListSelVo> getFavList(@RequestParam(defaultValue = "1") @Min(1) int page) {
         MyFavListSelDto dto = new MyFavListSelDto();
         dto.setPage(page);
         return service.selMyFavList(dto);
