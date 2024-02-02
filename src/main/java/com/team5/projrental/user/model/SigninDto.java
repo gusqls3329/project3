a1da1631e442062927c3e4e548e917c19049eb7b
firstt <khi05040@naver.com>
2024-02-02 17:05:27 +0900

    user dto수정

--- src/main/java/com/team5/projrental/user/model/SigninDto.java
+++ src/main/java/com/team5/projrental/user/model/SigninDto.java
@@ -11,7 +11,7 @@ import org.hibernate.validator.constraints.Length;
 @Data
 public class SigninDto {
     @NotBlank(message = ErrorMessage.CAN_NOT_BLANK_EX_MESSAGE)
-    @Length(min = 8, max = 15, message = ErrorMessage.ILLEGAL_RANGE_EX_MESSAGE)
+    @Length(min = 4, max = 15, message = ErrorMessage.ILLEGAL_RANGE_EX_MESSAGE)
     @Pattern(regexp =  "^\\w{4,15}$", message = ErrorMessage.NO_SUCH_USER_EX_MESSAGE)
     @Schema(defaultValue = "qwqwqw11")
     private String uid;
