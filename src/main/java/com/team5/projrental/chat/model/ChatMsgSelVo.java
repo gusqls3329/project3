462469abef50fdf828ac4f82415aff94955c182c
jo <yuyule20@naver.com>
2024-02-03 15:43:56 +0900

    2024-02-03 --byHyunmin

--- src/main/java/com/team5/projrental/chat/model/ChatMsgSelVo.java
+++ src/main/java/com/team5/projrental/chat/model/ChatMsgSelVo.java
@@ -1,6 +1,7 @@
 package com.team5.projrental.chat.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
@@ -10,5 +11,6 @@ public class ChatMsgSelVo {
     private String writerPic; // 메세지 보낸 사람 사진
     private String msg; // 메세지
     private String createdAt; // 각 seq에 대한 메세지 보낸 시간
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String prodPic; // 제품 대표사진
 }
