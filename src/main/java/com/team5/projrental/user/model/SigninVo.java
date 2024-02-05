2291f02050b97defd53499bb75790462f90010d8
jo <yuyule20@naver.com>
2024-02-05 16:30:57 +0900

    2024-02-05 --by Hyunmin

--- src/main/java/com/team5/projrental/user/model/SigninVo.java
+++ src/main/java/com/team5/projrental/user/model/SigninVo.java
@@ -1,11 +1,15 @@
 package com.team5.projrental.user.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SigninVo {
     @JsonIgnore
     private String upw;
