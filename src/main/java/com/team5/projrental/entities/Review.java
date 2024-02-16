40907eb1aa91c8acb2d4ae36514eb19d880ce47f
firstt <khi05040@naver.com>
2024-02-16 21:37:51 +0900

    리뷰

--- src/main/java/com/team5/projrental/entities/Review.java
+++ src/main/java/com/team5/projrental/entities/Review.java
@@ -2,6 +2,7 @@ package com.team5.projrental.entities;
 
 
 import com.team5.projrental.entities.ids.PaymentInfoIds;
+import com.team5.projrental.entities.inheritance.Users;
 import com.team5.projrental.entities.mappedsuper.UpdatedAt;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -21,10 +22,14 @@ public class Review extends UpdatedAt {
     private Long ireview;
 
 
+    @OneToOne
+    @JoinColumn(name = "ipayment")
+    private Payment payment;
 
-    private Long ipayment;
+    @ManyToOne
+    @JoinColumn(name = "iuser")
+    private Users users;
 
-    private Long iuser;
 
     @Column(length = 2000)
     private String contents;
