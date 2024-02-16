c2f32f310e6ab787f2329dea244994a3a75855e1
김현빈 <gusqls81017@gmail.com>
2024-02-16 17:33:55 +0900

    1

--- src/main/java/com/team5/projrental/entities/Comp.java
+++ src/main/java/com/team5/projrental/entities/Comp.java
@@ -3,7 +3,10 @@ package com.team5.projrental.entities;
 import com.team5.projrental.entities.enums.ProvideType;
 import com.team5.projrental.entities.inheritance.Users;
 import com.team5.projrental.entities.mappedsuper.BaseUser;
-import jakarta.persistence.*;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,12 +14,14 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class User extends Users {
-
+public class Comp  extends Users {
     @Embedded
     private BaseUser baseUser;
 
     @Enumerated(EnumType.STRING)
     private ProvideType provideType;
 
+    private Long compCode;
+    private String compNm;
+    private Long cash;
 }
