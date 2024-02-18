6ac5972fe4952ac6216a6e74202367b006aa65a9
devYule <privateDevYule@gmail.com>
2024-02-18 22:59:58 +0900

    2024-02-18 --by Hyunmin

---
+++ src/main/java/com/team5/projrental/entities/PaymentDetail.java
@@ -0,0 +1,33 @@
+package com.team5.projrental.entities;
+
+import com.team5.projrental.entities.enums.PaymentDetailCategory;
+import com.team5.projrental.entities.inheritance.Users;
+import com.team5.projrental.entities.mappedsuper.CreatedAt;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class PaymentDetail extends CreatedAt {
+
+    @Id
+    @Column(name = "iusers")
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn(name = "iusers")
+    @MapsId
+    private Users users;
+
+    private String tid;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentDetailCategory category;
+
+
+
+}
