d8780f19b35f8d02a9e1714ad2af8b4db8384da0
devYule <privateDevYule@gmail.com>
2024-02-19 15:47:49 +0900

    2024-02-19 --by Hyunmin

--- src/main/java/com/team5/projrental/user/UserRepository.java
+++ src/main/java/com/team5/projrental/user/UserRepository.java
@@ -1,8 +1,9 @@
 package com.team5.projrental.user;
 
+import com.team5.projrental.entities.User;
 import com.team5.projrental.user.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
