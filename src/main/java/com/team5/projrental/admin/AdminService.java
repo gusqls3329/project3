e8eda6e1512759eac91f42cbb00bbc3c0d57603b
김현빈 <gusqls81017@gmail.com>
2024-02-21 15:40:17 +0900

    현빈

---
+++ src/main/java/com/team5/projrental/admin/AdminService.java
@@ -0,0 +1,17 @@
+package com.team5.projrental.admin;
+
+import com.team5.projrental.admin.model.ProfitDto;
+import com.team5.projrental.admin.model.ProfitVo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+    public ProfitVo getProfit( ProfitDto dto){
+return null;
+    }
+}
