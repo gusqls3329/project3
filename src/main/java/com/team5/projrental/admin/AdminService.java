d699404e662437e15cf4ab05ef5d5590f58060a4
Carpediem2870 <luvrel@naver.com>
2024-02-22 18:15:51 +0900

    sangwon

--- src/main/java/com/team5/projrental/admin/AdminService.java
+++ src/main/java/com/team5/projrental/admin/AdminService.java
@@ -2,6 +2,7 @@ package com.team5.projrental.admin;
 
 import com.team5.projrental.admin.model.ProfitDto;
 import com.team5.projrental.admin.model.ProfitVo;
+import com.team5.projrental.payment.model.PaymentVo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ import org.springframework.stereotype.Service;
 public class AdminService {
 
     public ProfitVo getProfit(ProfitDto dto) {
+        return null;
+    }
+
+
+    public PaymentVo userGetPayment(long ipayment) {
+
         return null;
     }
 }
--- src/main/java/com/team5/projrental/websocket/RabbitConfig.java
+++ src/main/java/com/team5/projrental/websocket/RabbitConfig.java
@@ -19,29 +19,30 @@ import org.springframework.messaging.converter.StringMessageConverter;
 @EnableRabbit
 public class RabbitConfig {
 
+    //맘데로 해도되지만 현민씨가 정해놓기로함 보안상 이름우리가짓는게 좋음
     private static final String CHAT_QUEUE_NAME = "chat.queue";
     private static final String CHAT_EXCHANGE_NAME = "chat.exchange";
-    public static final String ROUTING_KEY = "room";
+    public static final String ROUTING_KEY = "room"; // 채팅방 찾아가도록 하는 기능 DEFAULT
 
     @Bean
     public Queue queue() {
-        return new Queue(CHAT_QUEUE_NAME, true);
+        return new Queue(CHAT_QUEUE_NAME, true); // 서버를 끌 때 Queue를 제거되지않도록 true
     }
 
     @Bean
-    public DirectExchange exchange() {
-        return new DirectExchange(CHAT_EXCHANGE_NAME);
+    public TopicExchange exchange() {
+        return new TopicExchange(CHAT_EXCHANGE_NAME); // Topic 사용
     }
 
-    @Bean
-    public Binding binding(Queue queue, DirectExchange exchange) {
+    @Bean // 바인딩 Queue를 to exchange
+    public Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
 
-    @Bean
+    @Bean // 컨버터
     public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory()); // 로그인정보 담겨있는거 사용선언
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter()); //메세지 컨버터 세팅된거 선언
         rabbitTemplate.setRoutingKey(CHAT_QUEUE_NAME);
         return rabbitTemplate;
     }
@@ -64,7 +65,7 @@ public class RabbitConfig {
         return factory;
     }
 
-    @Bean
+    @Bean // 이걸 레빗템플릿에 끼워넣는다고함..? 일단 이거그대로 사용
     public StringMessageConverter stringMessageConverter() {
         // String 으로 변환하는 컨버터 - 가장 문제가 적음.
         return new StringMessageConverter();
@@ -77,85 +78,3 @@ public class RabbitConfig {
 
 
 }
-
-
-
-/*import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.context.annotation.Bean;
-
-public class RabbitConfig {
-    private static final String CHAT_QUEUE_NAME = "chat.queue";
-    private static final String CHAT_EXCHANGE_NAME = "chat.exchange";
-    private static final String ROUTING_KEY = "room.*";
-
-
-    @Bean // Queue 등록
-    public Queue queue() {
-        return new Queue(CHAT_QUEUE_NAME, true);
-    }
-
-    @Bean // Exchange 등록
-    public TopicExchange exchange() {
-        return new TopicExchange(CHAT_EXCHANGE_NAME);
-    }
-
-    @Bean //Exchange와 Queue 바인딩
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
-    }
-
-    @Bean // messaheConverter를 커스터마이징 하기 위해 Bean 새로 등록
-    public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        rabbitTemplate.setRoutingKey(CHAT_QUEUE_NAME);
-        return rabbitTemplate;
-    }
-
-    @Bean
-    public SimpleMessageListenerContainer container() {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory());
-        container.setMessageListener(null);
-        return container;
-    }
-
-    //Spring에서 자동생성해주는 ConnectionFactory는 SimpleConnectionFactory인가? 그건데
-    //여기서 사용하는 건 CachingConnectionFacotry라 새로 등록해줌
-
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory factory = new CachingConnectionFactory();
-        factory.setHost("localhost");
-        factory.setUsername("guest");
-        factory.setPassword("guest");
-        return factory;
-    }
-
-
-    @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter() {
-        //LocalDateTime serializable을 위해
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
-        objectMapper.registerModule(dateTimeModule());
-
-        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter(objectMapper);
-
-        return converter;
-    }
-
-    @Bean
-    public Module dateTimeModule() {
-        return new JavaTimeModule();
-    }
-}*/
