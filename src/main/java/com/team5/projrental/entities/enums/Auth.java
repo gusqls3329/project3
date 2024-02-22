5ba57ef5f5362033f72cdf7e4d0e6d06475c446b
devYule <privateDevYule@gmail.com>
2024-02-22 21:34:50 +0900

    2024-02-22 --by Hyunmin

--- src/main/java/com/team5/projrental/entities/enums/Auth.java
+++ src/main/java/com/team5/projrental/entities/enums/Auth.java
@@ -1,7 +1,17 @@
 package com.team5.projrental.entities.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Auth {
-    ADMIN, USER, COMP;
+    ADMIN(3), USER(1), COMP(-1);
+
+    private int iauth;
+
+    Auth(int iauth) {
+        this.iauth = iauth;
+    }
+
 
     public static Auth getAuth(String role) {
         return role.equalsIgnoreCase("ROLE_COMP") ? Auth.COMP :
