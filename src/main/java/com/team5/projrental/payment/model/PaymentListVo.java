407492f5dac646b3bdd257a27fc96985f62845f8
김현빈 <gusqls81017@gmail.com>
2024-02-23 14:53:56 +0900

    현빈

--- src/main/java/com/team5/projrental/payment/model/PaymentListVo.java
+++ src/main/java/com/team5/projrental/payment/model/PaymentListVo.java
@@ -1,5 +1,6 @@
 package com.team5.projrental.payment.model;
 
+import com.team5.projrental.entities.enums.ProductStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,7 +12,7 @@ public class PaymentListVo {
     private Integer iuser;
     private String nick;
     private String userPic;
-
+    private ProductStatus productStatus;
     private Integer ipayment;
     private Integer iproduct;
     private String title;
@@ -21,6 +22,7 @@ public class PaymentListVo {
     private LocalDate rentalEndDate;
     private Integer rentalDuration;
     private Integer price;
+    private Integer totalPrice;
     private Integer deposit;
 
 }
